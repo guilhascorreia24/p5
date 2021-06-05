7518d5514f1b33581cb3c83452fa030fcdeca5af
guilhascorreia24 <55436745+guilhascorreia24@users.noreply.github.com>
2021-06-05 04:01:16 +0100

    t

--- Plataform.cpp
+++ Plataform.cpp
@@ -6,6 +6,7 @@
 #include <glm/gtx/io.hpp>
 #include "Object.h"
 #include "Plataform.h"
+#include "Scenery.h"
 #include <vector>
 #include <set>
 #include <algorithm>
@@ -21,6 +22,20 @@ Plataform::Plataform(const char *c, const char *t)
     setBlocks();
     Model=glm::mat4(1);
 }
+void Plataform::setLavaBlocks(Block b){
+    glm::vec3 t[7];
+    t[0]=glm::vec3(-2.3833,0,-1.3062);
+    t[1]=glm::vec3(-4.8184,0,-4.8996); //posiçoes do blocos no nivel 2
+    t[2]=glm::vec3(-6.0107,0,-0.083595);
+    t[3]=glm::vec3(-1.18759,0,2.29437);
+    t[4]=glm::vec3(2.39821,0,-1.30617);
+    t[5]=glm::vec3(6.01086,0,-2.50065);
+    t[6]=glm::vec3(2.4165,0,5.91033);
+    for(int i=0;i<7;i++){
+        this->lavaBlocks.push_back(b);
+        this->lavaBlocks.at(i).atual=this->lavaBlocks.at(i).inicial_pos=t[i];
+    }
+}
 
 void Plataform::setBlocks()
 {
@@ -29,7 +44,7 @@ void Plataform::setBlocks()
         superiorFaces(f);
     }
     findFinal();
-    //cout << blocks.size() << std::endl;
+    cout << blocks.size() << std::endl;
 }
 
 void Plataform::superiorFaces(struct Faces face)
@@ -59,7 +74,6 @@ void Plataform::superiorFaces(struct Faces face)
     vmin.y = min.y;
     vmin.z = zmin;
     struct Vertex m;
-    //cout<<vmin.tostring()<<"&"<<vmax.tostring()<<std::endl;
     m.x = (xmax + xmin) / 2;
     m.y = (max.y + min.y) / 2;
     m.z = (zmax + zmin) / 2;
@@ -82,14 +96,21 @@ bool Plataform::overBlock(Block b)
     }
     return false;
 }
+
+bool Plataform::overLavaBlocks(Block b){
+
+    for (Block b2 : lavaBlocks)
+    {
+        if (b.getcenter().x >= b2.min.x && b.getcenter().x <= b2.max.x && b.getcenter().z >= b2.min.z && b.getcenter().z <= b2.max.z && b.getcenter().y - b2.getcenter().y >= (b.height / 2) + (b2.height / 2))
+        {
+            return true;
+        }
+    }
+    return false;
+}
 void Plataform::findFinal()
 {
-    //printf("findfinal\n");
-    cout << blocks.at(0).width << " " << blocks.at(0).length << std::endl;
     int r = 1000000;
-    /*for(Block b:blocks){
-        cout<<b.tostring()<<std::endl;
-    }*/
     for (float i = min.x + blocks.at(0).length/2; i < max.x; i +=blocks.at(0).length )
     {
         for (float j = min.z + blocks.at(0).width/2; j < max.z; j += blocks.at(0).width )
@@ -126,7 +147,6 @@ void Plataform::findFinal()
             }
         }
     }
-      cout << final_.tostring() << std::endl;
 }
 bool Plataform::CheckIsBlock(Block o){
     for(Block b:blocks){
--- Scenery.cpp
+++ Scenery.cpp
@@ -54,12 +54,14 @@ Scenery::Scenery(glm::mat4 MVP, Block b, Plataform p, Plataform f)
     block.Model = block.Model * glm::rotate(glm::mat4(1), glm::radians(block.rotations[2]), glm::vec3(0, 0, 1));
 
     plat.MVP = MVP * glm::translate(glm::mat4(1), plat.atual);
-    //cout<<floor.atual<<std::endl;
     this->floor.atual[1] = -8;
     this->floor.MVP = MVP * glm::translate(glm::mat4(1), floor.atual);
     addPlataform(plat);
     objs.push_back(floor);
     block.setWalk(plat.blocks.at(0).width);
+    for(int i=0;i<plat.lavaBlocks.size();i++){
+        plat.lavaBlocks.at(i).MVP=MVP*glm::translate(glm::mat4(1),plat.lavaBlocks.at(i).inicial_pos);
+    }
     //addPlataform(floor);
 };
 bool Scenery::BlockOverEdgesPrataform()
@@ -84,7 +86,6 @@ bool Scenery::BlockOverEdgesPrataform()
     {
         if (block.rotate_lateral == glm::vec3(0, 1, 0))
         {
-
             return OntheBorders(frente, tras, glm::vec3(0, 0, block.height / 4));
         }
         else if (block.rotate_vertical == glm::vec3(0, 1, 0))
